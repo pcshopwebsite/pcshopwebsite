0b1a9cedf0eb431b1cf26ee6ef9686e531183022
Steve Nguyen <nhduc.seuit@gmail.com>
2023-05-27 16:03:23 +0700

    Trivial success try with a maven build approach copied from https://marco.dev/angular-with-java (Marco Molteni) Still not sure how it works with the git actions

--- backend/src/main/java/org/pcshopwebsite/Main.java
+++ backend/src/main/java/org/pcshopwebsite/Main.java
@@ -3,7 +3,6 @@ package org.pcshopwebsite;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 
-import ch.qos.logback.core.net.SyslogOutputStream;
 import com.github.javafaker.Faker;
 import com.github.javafaker.Lorem;
 import org.pcshopwebsite.product.Product;
@@ -12,22 +11,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Random;
 
 @SpringBootApplication
-@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-    }
-
-    @GetMapping("/")
-    public List<String> helloWorld() {
-        return List.of("Hello", "World");
     }
 
     @Bean
---
+++ backend/src/main/java/org/pcshopwebsite/UiController.java
@@ -0,0 +1,23 @@
+package org.pcshopwebsite;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UiController {
+    /**
+     * FIX WHITELABEL ERROR PAGE (404) WHEN REFRESHING THE PAGE
+     * "/{path:[^\\.]*}"): It maps any path segment (denoted by {path}) that does not contain a dot ([^\\.]) to the
+     * redirectApi() method.
+     * The regular expression [^\\.] ensures that the path segment does not contain a dot, effectively matching any
+     * path except those with file extensions.
+     *
+     * The forward:/ statement instructs Spring MVC to internally forward the request to the root path ("/"),
+     * effectively handling the request within the same server-side request context without a full browser redirect.
+     *
+     */
+    @GetMapping(value = "/{path:[^\\.]*}")
+    public String redirectApi() {
+        return "forward:/";
+    }
+}
