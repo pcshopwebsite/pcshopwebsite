9a90072cdb043589d3cd59dd900563c617848c9f
Steve Nguyen <nhduc.seuit@gmail.com>
2023-06-01 14:37:44 +0700

    Try to configure CI and CI/CD to dev workflows

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -5,16 +5,14 @@ APP_NAME="PCShopApp"
 JAR_FILE="PCShopApp-0.0.1.jar"
 LOG_FILE="application.log"
 
-export JAVA_OPTS="-Dspring.profiles.active=dev"
-
 # Check if the app is already running
 if pgrep -f $JAR_FILE >/dev/null; then
-    echo "The $APP_NAME is already running."
-    exit 0
+    echo "The $APP_NAME is already running. Will terminate"
+    ./application-stop.sh
 fi
 
 # Start the app in the background
-nohup java -jar /usr/local/codedeployresources/$JAR_FILE > $LOG_FILE 2>&1 &
+nohup java -jar /usr/local/codedeployresources/$JAR_FILE --spring.profiles.active=dev > $LOG_FILE 2>&1 &
 
 echo "Starting $APP_NAME..."
 echo "The application logs are being written to $LOG_FILE."
