32d331a0207670f961f78728655585bc21a17555
Steve Nguyen <nhduc.seuit@gmail.com>
2023-06-02 13:24:43 +0700

    Try to use static files inside spring boot app instead of jar package

--- backend/src/main/java/org/pcshopwebsite/PCShopApp.java
+++ backend/src/main/java/org/pcshopwebsite/PCShopApp.java
@@ -13,9 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Main {
+public class PCShopApp {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(PCShopApp.class, args);
     }
 
     @Bean
